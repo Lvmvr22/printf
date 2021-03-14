5dbc4ca2d2ea1ccd77ef4888d4200a19b968a1bc
Lvmvr22 <lamarnyairo@gmail.com>
2021-03-14 17:04:47 +0300

    Key File

---
+++ holberton.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(char *c);
+int _printf(const char *format, ...);
+void swapNum(char *x, char *y);
+char* revString(char *buffer, int i, int j);
+char* intasc(int value, char *buffer, int base);
+
+#endif
